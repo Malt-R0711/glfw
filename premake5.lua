83eabd388c2697424146a7630e4f601d7dea364c
Malt_R <malte.r@mail.de>
2021-01-05 23:25:22 +0100

    fixed wrong path to source files for windows platform

--- premake5.lua
+++ premake5.lua
@@ -29,20 +29,20 @@ project "GLFW"
 
         files
         {
-            "win32_platform.h",
-            "win32_joystick.h",
-            "wgl_context.h",
-            "egl_context.h",
-            "osmesa_context.h",
-            "win32_init.c",
-            "win32_joystick.c",
-            "win32_monitor.c",
-            "win32_time.c",
-            "win32_thread.c",
-            "win32_window.c",
-            "wgl_context.c",
-            "egl_context.c",
-            "osmesa_context.c"
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+            "src/wgl_context.h",
+            "src/egl_context.h",
+            "src/osmesa_context.h",
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
 
         defines
